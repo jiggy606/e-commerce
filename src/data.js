322e883dded01f1a3adbd3c1794a7a2867227132
Iberi Daniel <danieliberi99@gmail.com>
2022-06-22 18:15:41 +0100

    slider done

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const sliderItems = [
+    {
+        id: 1,
+        title: 'evening time',
+        desc: "don't waste time. get flat 300,000",
+        bg: "#f1eeee",
+        img: "https://i.ibb.co/cXFnLLV/3.png",
+    },
+    {
+        id: 2,
+        title: 'day time',
+        desc: "don't waste time. get flat 500,000",
+        bg: "#f1eeee",
+        img: "https://i.ibb.co/DG69bQ4/2.png",
+    }
+];
+
+export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+];
